01664423686f6bb551e6fe78234532ee509a1231
jmacey <jmacey@bournemouth.ac.uk>
2025-10-31 10:27:28 +0000

    Working DLA demo

--- DLA/DLA.py
+++ DLA/DLA.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env -S uv run --script
 #
+import argparse
 import random
+import sys
+from pathlib import Path
 
 from image import Image, rgba
 
@@ -9,19 +12,24 @@ class DLA:
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.image = Image(self.width, self.height)
-        self.image.clear((255, 255, 255, 255))
+        self.sim_data = Image(self.width, self.height, (255, 255, 255, 255))
+        self.output_image = Image(self.width, self.height, (128, 128, 128, 255))
 
     def save_image(self, fname: str) -> None:
-        self.image.save(fname)
+        self.output_image.save(fname)
 
-    def random_seed(self) -> tuple[int, int]:
+    def random_seed(self) -> None:
         """
         place a random seed inside the boundary with a=0
         """
         x = random.randint(1, self.width - 1)
         y = random.randint(1, self.height - 1)
-        self.image.set_pixel(x, y, (255, 255, 255, 0))
+        self.sim_data.set_pixel(x, y, (255, 255, 255, 0))
+        self.output_image.set_pixel(x, y, (0, 255, 0, 255))  # seeds are now green
+
+    def set_seed(self, x: int, y: int) -> None:
+        self.sim_data.set_pixel(x, y, (255, 255, 255, 0))
+        self.output_image.set_pixel(x, y, (0, 255, 0, 255))  # seeds are now green
 
     def _random_start(self) -> tuple[int, int]:
         x = random.randint(1, self.width - 1)
@@ -45,10 +53,11 @@ class DLA:
             else:  # check for seed
                 for x_offset in [-1, 0, 1]:
                     for y_offset in [-1, 0, 1]:
-                        pixel_colour = self.image.get_pixel(x + x_offset, y + y_offset)
+                        pixel_colour = self.sim_data.get_pixel(x + x_offset, y + y_offset)
                         if pixel_colour[3] == 0:
                             print("Found Seed")
-                            self.image.set_pixel(x, y, (255, 0, 0, 0))
+                            self.sim_data.set_pixel(x, y, (255, 0, 0, 0))
+                            self.output_image.set_pixel(x, y, (0, 0, 255, 255))
                             found = True
                             walking = False
                             break
@@ -58,16 +67,31 @@ class DLA:
         return found
 
 
-def main():
-    sim = DLA(512, 512)
-    for _ in range(20):
-        sim.random_seed()
-
+def main(output_dir: str, file_name: str, width: int, height: int):
+    sim = DLA(args.width, args.height)
+    # for _ in range(20):
+    #     sim.random_seed()
+    for x in range(width):
+        sim.set_seed(x, height // 2)
+    frame_number = 0
+    found_count = 0
     for _ in range(10000):
         if sim.walk():
             print("Found a seed")
-    sim.save_image("test.png")
+            found_count += 1
+            if found_count % 10 == 0:
+                file = Path(output_dir) / f"{file_name}.{frame_number:04}.png"
+                sim.save_image(file)
+                frame_number += 1
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Simple DLA Simulation")
+    parser.add_argument("-o", "--output", type=str, default="./", help="output dir defaults to current")
+    parser.add_argument("-n", "--name", type=str, default="Sim", help="output name for file")
+    parser.add_argument("-ht", "--height", type=int, default=400, help="height of sim")
+    parser.add_argument("-w", "--width", type=int, default=400, help="height of sim")
+
+    args = parser.parse_args()
+    Path(args.output).mkdir(parents=True, exist_ok=True)
+    main(args.output, args.name, args.width, args.height)
